1ade1234f42ff2e9d4572bb078e0d37d878d5b58
colesayer <colesayershapiro@gmail.com>
2017-10-12 21:18:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(date){
+    return date - this.startDate.getFullYear() -1;
+  }
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    const a = eastWest.indexOf(this.beginningLocation["horizontal"]);
+    const b = eastWest.indexOf(this.endingLocation["horizontal"]);
+    const c = this.beginningLocation["vertical"];
+    const d = this.endingLocation["vertical"];
+    const horizontalBlocksTravelled = a >= b ? (a-b) : (b-a);
+    const verticalBlocksTravelled = c >= d ? (c-d) : (d-c)
+    return horizontalBlocksTravelled + verticalBlocksTravelled
+  }
+
+  estimatedTime(peak = false){
+    const blocks = this.blocksTravelled()
+    const d = new Date();
+    const n = d.getHours();
+    if (peak === false){
+      return blocks/3
+    } else {
+      return blocks/2
+    }
+    // if (n >= 7 && n <= 9 || n >= 19 && n <= 21){
+    //   return blocks/2
+    // } else {
+    //   return blocks/3
+    // }
+  }
+
+}
